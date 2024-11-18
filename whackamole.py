2a937c35cec7c981a7e409a5629eafd4c00bc71f
elsam <elsamathew005@gmail.com>
2024-11-18 15:18:36 -0500

    Finished lab- mole moves in grid lines

--- whackamole.py
+++ whackamole.py
@@ -6,7 +6,6 @@ def main():
     try:
         pygame.init()
         # You can draw the mole with this snippet:
-        #screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
         mole_image = pygame.image.load("mole.png")
         screen = pygame.display.set_mode((640, 512))
         clock = pygame.time.Clock()
@@ -18,32 +17,19 @@ def main():
                 if event.type == pygame.QUIT:
                     running = False
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-                    # mole_image.get_rect(center=(x, y))
-                    print(event.pos[0])
                     if x // 32 * 32 == event.pos[0] // 32 * 32 and y // 32 * 32 == event.pos[1] // 32 * 32:
-                        x = random.randrange(0, 609)
-                        y = random.randrange(0, 481)
+                        x = random.randrange(0, 609) // 32 * 32
+                        y = random.randrange(0, 481) // 32* 32
                         screen.blit(mole_image, mole_image.get_rect(topleft = (x, y)))
 
-            screen.fill("light green")
+            screen.fill((242, 255, 244))
             for i in range(1, 20):
-                pygame.draw.line(screen, "black", (i * 32, 0), (i * 32, 512))
+                pygame.draw.line(screen, (15, 79, 21), (i * 32, 0), (i * 32, 512))
             for j in range(1, 16):
-                pygame.draw.line(screen, "black", (0, j * 32), (640, j * 32))
+                pygame.draw.line(screen, (15, 79, 21), (0, j * 32), (640, j * 32))
             screen.blit(mole_image, mole_image.get_rect(topleft=(x, y)))
             pygame.display.flip()
             clock.tick(60)
-            # if pygame.MOUSEBUTTONDOWN:
-            #     a, b = event.pos
-            #     print(x)
-            #     x = random.randrange(0, 609)
-            #     y = random.randrange(0, 481)
-            #     if x % 32 != 0:
-            #         x = x // 32
-            #     if y % 32 != 0:
-            #         y = y // 32
-            #     screen.blit(mole_image, mole_image.get_rect(topleft= (x, y)))
-
     finally:
         pygame.quit()
 
